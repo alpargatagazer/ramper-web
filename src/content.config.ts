1845d9343f0ce88fc5271e8bc4b3c5177e733053
Ángel Oreste <oresgaze@gmail.com>
2026-05-27 15:30:27 +0200

    ASTRO6: working

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 // src/content.config.ts
 
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
 // 1. News Posts Collection
