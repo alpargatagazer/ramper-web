32658066ab8204f53cdacf9f99aab477c4560caf
Ángel Oreste <oresgaze@gmail.com>
2026-07-07 13:29:54 +0200

    NEWSLETTER-1: fixed Sonar

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,7 +4,8 @@ export const prerender = true;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapUrl = site ? new URL('sitemap-index.xml', site).href : '';
-  const body = `User-agent: *\nAllow: /${sitemapUrl ? `\n\nSitemap: ${sitemapUrl}` : ''}`;
+  const sitemapLine = sitemapUrl ? `\n\nSitemap: ${sitemapUrl}` : '';
+  const body = `User-agent: *\nAllow: /${sitemapLine}`;
   return new Response(
     body,
     {
